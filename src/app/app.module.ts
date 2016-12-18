3bb70fbcc83c4c4f1b403dd0481e7a1be17996aa
arielisaacs <ariel.isaacs@lucidworks.com>
2016-12-18 00:25:17 -0800

    add home and prelim routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes)
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
