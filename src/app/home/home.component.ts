9951ae3831dd74db6912f6d4396a685579431a46
Ariel Isaacs <ariel.isaacs@lucidworks.com>
2017-01-15 23:01:35 -0800

    use better carousel, fix border

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 // import { CarouselModule } from '@ng2-bootstrap/ng2-bootstrap/carousel';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class HomeComponent implements OnInit {
 
